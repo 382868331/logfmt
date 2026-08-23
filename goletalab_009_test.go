4812186e14d22e0275685be69ebd503b2c43ffaf
382868331 <382868331@users.noreply.github.com>
2026-08-24 02:05:08 +0800

    fix case 009: 空键验证

--- goletalab_009_test.go
+++ goletalab_009_test.go
@@ -11,3 +11,10 @@ func TestGoletaLogfmt009(t *testing.T) {
 		t.Fatalf("err=%v", e)
 	}
 }
+
+func TestGoletaLogfmt009Boundary(t *testing.T) {
+	_, e := MarshalKeyvals("	", 1)
+	if !errors.Is(e, ErrInvalidKey) {
+		t.Fatalf("err=%v", e)
+	}
+}
